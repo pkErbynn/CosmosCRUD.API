318af9edb1ba1c595de4dff5148c1c829a66a492
erbynn <john.erbynn@gmail.com>
2021-02-15 07:06:34 +0000

    read data with queryable api

--- CosmosCRUDConsole.API/Program.cs
+++ CosmosCRUDConsole.API/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.Azure.Documents.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 //This demo dives into leveraging Cosmos SDK to;
 //- create databases and collections
@@ -16,56 +17,53 @@ namespace CosmosCRUDConsole.API
 {
     class Program
     {
-        private static readonly string CosmosEndpoint = "https://localhost:8081";
-        private static readonly string EmulatorKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
-        private static readonly string DatabaseId = "Tourism";
-        private static readonly string CastlesCollection = "Castles";
+      
 
         static void Main(string[] args)
         {
-            // Create the client connection
-            var client = new DocumentClient(
-                new Uri(CosmosEndpoint),
-                EmulatorKey,
-                new ConnectionPolicy
-                {
-                    ConnectionMode = ConnectionMode.Direct,
-                    ConnectionProtocol = Protocol.Tcp
-                });
 
-            // Set up database and collection Uris
-            var databaseUri = UriFactory.CreateDatabaseUri(DatabaseId);
-            var castleCollectionUri = UriFactory.CreateDocumentCollectionUri(DatabaseId, CastlesCollection);
-            var castlesCollection = new DocumentCollection { Id = CastlesCollection };
+// DB SETUPS
+            var cosmosService = new CosmosConfigService();
 
-            // Create a new db in cosmos
-            client.CreateDatabaseIfNotExistsAsync(new Database { Id = DatabaseId }).Wait();  // .Result makes it sync
+            //var db = cosmosService.VarifyDatabaseCreated();
+            //var coll = cosmosService.VerifyCollectionCreation();
+            //var doc = cosmosService.VerifyDocumentCreation(new Castle { Name = "Elmina Castle" });
+            //var doc2 = cosmosService.VerifyDocumentCreation(new Castle { Name = "Cape Coast"});
 
-            // Create new Container(Collection/Table) inside db to store Landmarks
-             client.CreateDocumentCollectionIfNotExistsAsync(databaseUri, castlesCollection).Wait(); ;
+            //Task.FromResult(db, coll, doc);
+
+            //Console.WriteLine("IsDBCreated: " + db.Result);
+            //Console.WriteLine("IsCollectionCreated: " + coll.Result);
+            //Console.WriteLine("IsDocumentCreated: " + doc2.Result);
+
+// FETCH DATA
+            //var castles = cosmosService.GetItemsAsync<Castle>(e => e.Name == "Elmina Castle").Result;
+            //Console.WriteLine("BEFORE 4EACH");
+            //Console.WriteLine("Count: ", castles.ToList().Count);
+            //castles.ToList().ForEach(Console.WriteLine);
+
+            //var castleMatches = cosmosService.GetItemsAsync(e => e.Name.StartsWith("El")).Result;
+            //castleMatches.ToList().ForEach(Console.WriteLine);
+
+            //var r3 = cosmosService.GetCastleQuery();
+            //Console.WriteLine(r3.Count());
+            //r3.ToList().ForEach(Console.WriteLine);
+
+            var r4 = cosmosService.GetByName("Elmina Castle");
+            Console.WriteLine(r4.Count());
+            r4.ToList().ForEach(Console.WriteLine);
+
+            //var r = cosmosService.GetData<Castle>().Result;
+            //Console.WriteLine(r.ToString());
+            //Console.WriteLine(r.Result);
 
 
-            // Create Item/Document/Row in Collection to be stored...specifying db and collection to target
-            var elCastle = new Castle { Name = "Elmina Castle" };
+// DELETE DATA
+            //string docId = "0ad0182f-847f-475b-8486-d16fea58e37a";
+            //var r2 = cosmosService.DeleteUserAsync(docId).Result;
+            //Console.WriteLine(r2);
 
-            client.CreateDocumentAsync(castleCollectionUri, elCastle).Wait();
-
-
-            // READING DATA....USING STRING QUERY
-
-            var cosmosService = new CosmosService<Castle>
-            {
-                DocumentClient = client,
-                DatabaseId = DatabaseId,
-                CollectionId = CastlesCollection
-            };
-
-            var castles = cosmosService.GetItemsAsync(e => e.Name == "Elmina Castle").Result;
-            Console.WriteLine("BEFORE 4EACH");
-            castles.ToList().ForEach(Console.WriteLine);
-
-            var castleMatches = cosmosService.GetItemsAsync(e => e.Name.StartsWith("El")).Result;
-            castleMatches.ToList().ForEach(Console.WriteLine);
+            Console.Read();
         }
     }
 }
---
+++ CosmosCRUDConsole.API/Services/CosmosConfigService.cs
@@ -0,0 +1,159 @@
+﻿using CosmosCRUDConsole.API.Models;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosCRUDConsole.API.Services
+{
+    public class CosmosConfigService
+    {
+        private static readonly string CosmosEndpoint = "https://localhost:8081";
+        private static readonly string EmulatorKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+        private static readonly string DatabaseId = "Tourism";
+        private static readonly string CollectionId = "Castles";
+
+        private readonly DocumentClient client;
+
+
+        public CosmosConfigService()
+        {
+            client = new DocumentClient(
+               new Uri(CosmosEndpoint),
+               EmulatorKey,
+               new ConnectionPolicy
+               {
+                   ConnectionMode = ConnectionMode.Direct,
+                   ConnectionProtocol = Protocol.Tcp
+               });
+        }
+
+        public async Task<bool> VarifyDatabaseCreated()
+        {
+            try
+            {
+                await client.CreateDatabaseIfNotExistsAsync(new Database { Id = DatabaseId });
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> VerifyCollectionCreation()
+        {
+            var databaseUri = UriFactory.CreateDatabaseUri(DatabaseId);
+            var castlesCollection = new DocumentCollection { Id = CollectionId };
+
+            try
+            {
+                await client.CreateDocumentCollectionIfNotExistsAsync(databaseUri, castlesCollection);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> VerifyDocumentCreation<T>(T documentName) where T : class
+        {
+            var castleCollectionUri = UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId);
+
+            try
+            {
+                await client.CreateDocumentAsync(castleCollectionUri, documentName);  // upsert 
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+         // FETCH
+        public IQueryable<Castle> GetCastleQuery()
+        {
+            return client.CreateDocumentQuery<Castle>(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId),
+                new FeedOptions { MaxItemCount = 20 });
+        }
+
+        public IQueryable<Castle> GetByName(string name)
+        {
+            return client.CreateDocumentQuery<Castle>(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId),
+                new FeedOptions { MaxItemCount = 3 }).Where((i) => i.Name == name);
+        }
+
+        public async Task<IEnumerable<T>> GetItemsAsync<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            var docCollectionUri = UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId);
+            var query = client.CreateDocumentQuery<T>(docCollectionUri)     // creating doc query from collection
+                                        .Where(predicate)
+                                        .AsDocumentQuery();
+
+          
+              var results = new List<T>();
+
+            while (query.HasMoreResults)
+            {
+                results.AddRange(await query.ExecuteNextAsync<T>());
+            }
+
+            return results;
+        }
+
+        // not recommended 
+        //public async Task<dynamic> GetData<T>() where T : class
+        //{
+        //    try
+        //    {
+        //        var doc = await client.ReadDocumentFeedAsync(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId),
+        //            new FeedOptions { MaxItemCount = 2 });
+
+        //        return doc;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("Error occured: " + ex.Message); ;
+        //        return null;
+        //    }
+        //}
+
+
+        // DELETE
+        public async Task<Castle> DeleteUserAsync(string id)
+        {
+            try
+            {
+                var collectionUri = UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id);
+
+                var result = await client.DeleteDocumentAsync(collectionUri);
+
+                return (dynamic)result.Resource;
+            }
+            catch (DocumentClientException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occured: "); ;
+                return null;
+            }
+        }
+
+        
+
+
+        // continue explore here - https://github.com/nhandrew/cosmosdbrestapi/blob/master/Controllers/ItemController.cs
+    }
+}
